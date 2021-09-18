4365e5223d20b1f2a436dee8dffc93c5cdde4828
LongFei Chen <git@longfeichen.ca>
2021-09-17 22:38:20 -0400

    experimental

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-var express = require('express');
-var multer = require('multer');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const path = require('path');
 
 var app = express();
 app.use(cors());
 
-var storage = multer.diskStorage({
+var storage_htm = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, 'public/uploads');
     },
     filename: function (req, file, cb){
-        cb(null, Date.now() + '-' + file.originalname);
+        cb(null,file.originalname);
     }
 })
 
-var upload = multer({storage: storage}).single('file');
+var storage_af = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'public/uploads/active_files');
+    },
+    filename: function (req, file, cb){
+        cb(null,file.originalname);
+    }
+})
 
-app.post('/upload', function (req, res){
-    upload(req, res, function (err){
+var upload_htm = multer({storage: storage_htm}).single('file');
+var upload_af = multer({storage: storage_af}).single('file');
+
+app.post('/upload/htm', function (req, res){
+    upload_htm(req, res, function (err){
         if(err instanceof multer.MulterError){
             return res.status(500).json(err);
         }else if(err){
@@ -25,6 +36,21 @@ app.post('/upload', function (req, res){
         }
         return res.status(200).send(req.file);
     })
+})
+
+app.post('/upload/af', function (req, res){
+    upload_af(req, res, function (err){
+        if(err instanceof multer.MulterError){
+            return res.status(500).json(err);
+        }else if(err){
+            return res.status(500).json(err);
+        }
+        return res.status(200).send(req.file);
+    })
+})
+
+app.get("/view", function (req, res){
+    res.sendFile(path.join(__dirname, '/public/uploads/active.htm'));
 })
 
 app.listen(8000, function(){
--- src/components/fileUpload/fileUpload.js
+++ src/components/fileUpload/fileUpload.js
@@ -12,11 +12,11 @@ export default function FileUpload(){
         setFile(e.target.files[0])
     }
 
-    function onClickHandler(e){
+    function onClickHandler(e, url){
         const data = new FormData();
         data.append('file', file);
-
-        axios.post("http://localhost:8000/upload", data, {})
+        console.log("uploading...");
+        axios.post(url, data, {})
         .then(res => {
             console.log(res.statusText);
         })
@@ -26,11 +26,20 @@ export default function FileUpload(){
         <div id="upload">
                 <form method="post" action="#" id="#">
                     <div class="form-group files color">
-                        <h2>Upload Your File </h2>
+                        <h2>Upload htm file </h2>
                         <br/>
                         <input type="file" name="file" className="form-control" onChange={(e)=>{onChangeHandler(e)}}/>
                         <br/>
-                        <input type="button" className="btn btn-success btn-block" value="Upload" onClick={(e)=>{onClickHandler(e)}}/>
+                        <input type="button" className="btn btn-success btn-block" value="Upload" onClick={(e)=>{onClickHandler(e, "http://localhost:8000/upload/htm")}}/>
+                    </div>                    
+                </form>
+                <form method="post" action="#" id="#">
+                    <div class="form-group files color">
+                        <h2>Upload active-files </h2>
+                        <br/>
+                        <input type="file" name="file" className="form-control" multiple onChange={(e)=>{onChangeHandler(e)}}/>
+                        <br/>
+                        <input type="button" className="btn btn-success btn-block" value="Upload" onClick={(e)=>{onClickHandler(e, "http://localhost:8000/upload/af")}}/>
                     </div>                    
                 </form>
         </div>
